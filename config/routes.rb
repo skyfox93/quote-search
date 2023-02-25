ad102486418f50691874d5548a20cc25484ce3ee
Skylar Salerno <skylar.salerno@gmail.com>
2023-02-24 22:16:41 -0500

    update root to star trek quotes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :dialogues
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "quotes#index"
+  root "dialogues#index"
   resources :quotes
 end
