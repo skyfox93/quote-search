5f5673716ad8232ae50b705ed6324d9096005276
Skylar Salerno <skylar.salerno@gmail.com>
2023-03-20 22:52:35 -0400

    higlighting and readbility

--- app/controllers/dialogues_controller.rb
+++ app/controllers/dialogues_controller.rb
@@ -10,7 +10,7 @@ class DialoguesController < ApplicationController
   def show
     @dialogue = Dialogue.find(params[:id])
     @text = EpisodeText.where(episode: @dialogue.episode, series: @dialogue.series).first().text
-    @with_highlights = @text.gsub("#{@dialogue.character} #{@dialogue.line}", "<span id=#{@dialogue.id} style=\"background-color: yellow\">#{@dialogue.character}: #{@dialogue.line}</span>")
+    @with_highlights = @text.gsub("#{@dialogue.character}: #{@dialogue.line}", "<span id=#{@dialogue.id} class=\"highlighted\">#{@dialogue.character}: #{@dialogue.line}</span>")
   end
 
   # GET /dialogues/new
